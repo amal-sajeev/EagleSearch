cd48dbaae512774c51919fc20ae2ad90886bd886
Amal Sajeev <amsajeev333@gmail.com>
2025-01-18 14:23:40 +0530

    Branded search to Eaglesearch

--- pdftest.py
+++ pdftest.py
@@ -1,4 +1,4 @@
-from processor import PDFProcessor
+from eaglesearch import Eaglesearch
 
 cruncher = PDFProcessor(qdrant_url ="https://c8abf992-e97b-4ccd-a2b0-046e5c9f5ee5.europe-west3-0.gcp.cloud.qdrant.io",qdrant_api_key= "iL-BfHumRrC_Ph8rOWLpGPT1JZu-8W6zfxxe1wm5cPaUiUeX3QSevg", collection_name="pdftest")
 
